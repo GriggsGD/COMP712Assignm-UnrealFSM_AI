9bac186c438800e4e0dba75fa0924bd06e40b142
Drew Tavares <79928221+GriggsGD@users.noreply.github.com>
2025-03-15 19:05:19 +0000

    Setup Combat Interface & Health Component
    - Developed Combat Interface for damage system
    - Developed Health Component for Actors to manage health systems

---
+++ Source/FSMSys/Private/HealthComponent.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthComponent.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	CurrentHealth = MaxHealth;
+}
+
+
+void UHealthComponent::TakeDamage(float DamageAmount)
+{
+	if (CurrentHealth <= 0) return;
+	CurrentHealth = FMath::Clamp(CurrentHealth - DamageAmount, 0.f, MaxHealth);
+	OnHealthChanged.Broadcast(CurrentHealth);
+
+	if (CurrentHealth <= 0)
+	{
+		OnDeath.Broadcast();
+	}
+}
+
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
---
+++ Source/FSMSys/Public/CombatInterface.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "CombatInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UCombatInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+class FSMSYS_API ICombatInterface
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Attack() = 0;
+	virtual void TakeDamage(float Damage) = 0;
+	virtual void Kill() = 0;
+};
